bd3dfa5ee813e39a5b759288a1e98757cc22c2ff
TheRealKevBot <Kglas001@gmail.com>
2021-08-03 17:37:40 -0600

    First

---
+++ index.js
@@ -0,0 +1,13 @@
+// Implement a function that receives two IPv4 addresses, and returns the number of addresses between them 
+// (including the first one, excluding the last one).
+
+// All inputs will be valid IPv4 addresses in the form of strings. The last address will always be greater than the first one. 
+
+function ipsBetween(start, end){
+    //TODO
+} 
+
+console.log(ipsBetween("10.0.0.0", "10.0.0.50"), 50);
+console.log(ipsBetween("20.0.0.10", "20.0.1.0"), 246); 
+
+
