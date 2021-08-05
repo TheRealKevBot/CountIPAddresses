32f451bbfd862230b9655ccf9011986e6a616a79
TheRealKevBot <Kglas001@gmail.com>
2021-08-05 15:17:16 -0600

    Build

--- index.js
+++ index.js
@@ -3,11 +3,29 @@
 
 // All inputs will be valid IPv4 addresses in the form of strings. The last address will always be greater than the first one. 
 
+
+//  Helpful Hint 
+//  An IPv4 address has the following format: x . x . x . x 
+//  where x is called an octet and must be a decimal value between 0 and 255. 256 in total! 
+
+
 function ipsBetween(start, end){
-    //TODO
+    let change = 0 
+    start = start.split('.').reverse()
+    end = end.split('.').reverse()
+    let p = 0
+    for (var i = 0; i < start.length; i++) {
+        i === 0 ? change += end[i] - start[i] 
+        : change += (end[i] - start[i]) * Math.pow(256, p)
+        p++
+    }
+    return change
 } 
 
+console.log(ipsBetween("10.0.0.0", "10.0.0.0"), 0);
 console.log(ipsBetween("10.0.0.0", "10.0.0.50"), 50);
 console.log(ipsBetween("20.0.0.10", "20.0.1.0"), 246); 
+console.log(ipsBetween("180.0.0.0", "181.0.0.0"), 16777216); 
+
 
 
